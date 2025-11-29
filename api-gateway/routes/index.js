f9d8539096675cf8b7004c202a4ba5771d18816f
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2025-11-29 17:09:12 +0530

    update gateway

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -3,63 +3,102 @@ import dotenv from "@dotenvx/dotenvx"
 dotenv.config()
 import {createProxyMiddleware} from "http-proxy-middleware"
 import { AuthVerify } from "../middelware/AuthVerify.js";
+import http from "http"
 
 const routes = express.Router()
 
+const agent = new http.Agent({
+  keepAlive: true,
+  maxSockets: 50,
+  maxFreeSockets: 20,
+});
 
 routes.use("/auth",createProxyMiddleware({
     target:process.env.auth_url,
     changeOrigin:true,
+    agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/merchant",AuthVerify,createProxyMiddleware({
     target:process.env.merchant_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/student",AuthVerify,createProxyMiddleware({
     target:process.env.student_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/course",AuthVerify,createProxyMiddleware({
     target:process.env.course_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/payment",AuthVerify,createProxyMiddleware({
     target:process.env.payment_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/logs",AuthVerify,createProxyMiddleware({
     target:process.env.activity_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/other",AuthVerify,createProxyMiddleware({
     target:process.env.other_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/notification",createProxyMiddleware({
     target:process.env.notification_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/content",createProxyMiddleware({
     target:process.env.content_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/reviews",createProxyMiddleware({
     target:process.env.reviews_url,
     changeOrigin:true,
+    agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/reports",createProxyMiddleware({
     target:process.env.reports_url,
-    changeOrigin:true
+    changeOrigin:true,
+    agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 
@@ -69,6 +108,9 @@ routes.use("/open/course",(req,res,next)=>{
 },createProxyMiddleware({
     target:process.env.course_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/open/merchant",(req,res,next)=>{
@@ -77,6 +119,9 @@ routes.use("/open/merchant",(req,res,next)=>{
 },createProxyMiddleware({
     target:process.env.merchant_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 routes.use("/open/other",(req,res,next)=>{
@@ -85,6 +130,9 @@ routes.use("/open/other",(req,res,next)=>{
 },createProxyMiddleware({
     target:process.env.other_url,
     changeOrigin:true,
+     agent,
+    proxyTimeout:10000,
+    timeout:10000,
 }))
 
 
