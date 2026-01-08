0fe789e858462ed82ca94a29cec4212bbdd05ebf
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2026-01-08 10:46:28 +0530

    udpate:

--- services/user-service/controllers/users/index.js
+++ services/user-service/controllers/users/index.js
@@ -117,6 +117,17 @@ export const getProfileById = async (req, res) => {
   }
 };
 
+export const GetUserProfileById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await UserModel.findOne({ _id: id }).select("-password")
+
+        res.status(200).json({ status: true, message: "profile data fetched", user })
+    } catch (error) {
+        res.status(500).json({ status: false, message: error.message })
+    }
+}
+
 
 export const UpdateProfileComplted = async (req, res) => {
     try {
